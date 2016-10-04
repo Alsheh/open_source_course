f2487aad4e3f660674bb5c013c436238d33a5b13
Alsheh <alsheh@rpi.edu>
2016-10-04 13:14:16 -0400

    Adding results to lab05report after running cmake and make

--- Lab05/tutorial.c
+++ Lab05/tutorial.c
@@ -6,10 +6,15 @@
 
 int main (int argc, char *argv[])
 {
+
   if (argc < 2)
     {
-    fprintf(stdout,"Usage: %s number\n",argv[0]);
-    return 1;
+      fprintf(stdout,"%s Version %d.%d\n",
+	      argv[0],
+	      Tutorial_VERSION_MAJOR,
+	      Tutorial_VERSION_MINOR);
+      fprintf(stdout,"Usage: %s number\n",argv[0]);
+      return 1;
     }
   double inputValue = atof(argv[1]);
   double outputValue = sqrt(inputValue);
